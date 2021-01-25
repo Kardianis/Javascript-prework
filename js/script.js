d546e9ab426443ebc3cb29c110fbb84fdb0deddb
Kardianis <michal_kuszneruk@tlen.pl>
2021-01-25 21:36:27 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,19 +2,23 @@ var computerMove;
 computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
-var playerMove;
-playerMove = 'papier'
+var playerMove, playerInput;
+playerInput = prompt ('wybierz swór ruch: 1:papier, 2:nożyce, 3:kamień')
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1'){playerMove = 'papier'; }
+else if (playerInput == '2') {playerMove = 'nożyce' ;}
+else if (playerInput == '3') {playerMove = 'kamień' ;}
+printMessage('Twój ruch: ' + playerMove);
+
 
 var randomNumber, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
     computerMove = 'kamień';
-  } 
-else if (randomNumber == "2") {
-    computerMove = "papier"
-}
-else if (randomNumber == "3") {
-    computerMove = "nożyce"
-} 
+  } else if (randomNumber == "2") {
+    computerMove = "papier" ;
+} else if (randomNumber == "3") {
+    computerMove = "nożyce" ;
+} ;
   printMessage('Mój ruch: ' + computerMove);
